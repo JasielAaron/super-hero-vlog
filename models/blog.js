1093d615a6e16f326b0c2fb41dea108ff94ce801
JasielAaron <150881134+JasielAaron@users.noreply.github.com>
2024-04-03 19:41:55 -0700

    started code for blogjs

---
+++ models/blog.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {
+
+}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+
+    },
+    create_on_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'username',
+        key: 'id',
+      },
+
+    },
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+
+  }
+);
+
+module.exports = Blog;
