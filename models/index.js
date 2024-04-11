191fca0533eeb20c6c92872214f840338da55d33
Trevor Johnson <trevor9j@protonmail.com>
2024-04-10 22:56:21 -0700

    updated blog

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const User = require('./user');
 const Blog = require('./blog');
-const Post = require('./post')
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
@@ -11,11 +10,4 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.hasMany(Post, {
-  foreignKey: 'user_id'
-});
-
-Post.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-module.exports = { User, Blog, Post };
+module.exports = { User, Blog };
--- public/js/login.js
+++ public/js/login.js
@@ -1,51 +1,53 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username= document.querySelector('#username').value.trim();
-    const password = document.querySelector('#Password').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+  console.log('-------bang-------')
+  const username = document.querySelector('#username');
+  const password = document.querySelector('#password');
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#username-signup');
+  const email = document.querySelector('#email-signup');
+  const password = document.querySelector('#password-signup');
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username: name.value, email: email.value, password: password.value }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+  }
+};
+
+document
+  .querySelector('#login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupFormHandler);
+
